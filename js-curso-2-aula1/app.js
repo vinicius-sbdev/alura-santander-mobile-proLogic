9e8b12754c5d73995ef79f4b76698c6e36044523
Noaru35 <vinicius.s.bastos55@gmail.com>
2025-10-13 19:47:17 -0300

    commit

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -1,19 +1,33 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1', 'Jogo do número Secreto')
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10')
+exibirTextoNaTela('h1', 'Jogo do número Secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute(){
     let chute = document.querySelector('input').value;
+    if (chute == numeroSecreto){
+        exibirTextoNaTela('h1', 'Acertou');
+        let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa"
+        let mensagem = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
+        exibirTextoNaTela('p', mensagem);
+    } else {
+        if (chute > numeroSecreto){
+            exibirTextoNaTela('p', 'O número secreto é menor!');
+        } else {
+            exibirTextoNaTela('p', 'O numéro secreto é maior!');
+        }
+        tentativas++;
+    }
     console.log(chute == numeroSecreto);
 }
 
-function gerarNumeroAleatorio(){
+function gerarNumeroAleatorio(){    
     return parseInt(Math.random() * 10 + 1);
 }
 
