2dd2ea4d81bc733bb6e8d374740db2166f0a349a
vinicius-sbdev <vinicius.sbdev@gmail.com>
2025-10-17 19:58:20 -0300

    commit

--- desafios/curso_2/desafio03/app.js
+++ desafios/curso_2/desafio03/app.js
@@ -1,3 +1,5 @@
+//Desafio 01 - Calculo IMC
+
 function calcularIMC(altura, peso){
     let imc = peso / (altura**2);
     return imc.toFixed(2)
@@ -16,4 +18,35 @@ function exibirNaTela(){
 
     let imc = calcularIMC(altura, peso);
     resultado.innerHTML = `O seu IMC é <strong>${imc}</strong>`;
+}
+
+//Desafio 02 - Valor fatorial
+
+function valorFatorial() {
+    let fatorial = parseInt(document.getElementById('fatorial').value);
+    let resultado_fatorial = document.getElementById('resultado_fatorial');
+
+    // Verificação de entrada
+    if (isNaN(fatorial) || fatorial < 0) {
+        resultado_fatorial.innerHTML = 'Por favor, insira um número inteiro positivo.';
+        return;
+    }
+
+    // Caso o número seja 0 ou 1, o fatorial é 1
+    if (fatorial === 0 || fatorial === 1) {
+        resultado_fatorial.innerHTML = `O fatorial de ${fatorial} é 1.`;
+        return;
+    }
+
+    // Cálculo do fatorial
+    let resultado = 1;
+    let contador = fatorial;
+
+    while (contador > 1) {
+        resultado *= contador;
+        contador--;
+    }
+
+    // Exibição final
+    resultado_fatorial.innerHTML = `O fatorial de ${fatorial} é ${resultado}.`;
 }
